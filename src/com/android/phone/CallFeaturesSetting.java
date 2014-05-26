28c9e4d14f69395485730009df0497ea9d3aa9a1
lichti1901 <lichti190185@gmail.com>
2014-05-26 20:03:25 +0200

    Add transparency to Glowpad background [3/3] Adds the option to make glowpad view background transparent on incoming call so the caller's entire picture can be seen.

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -192,6 +192,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private static final String BUTTON_CALL_UI_IN_BACKGROUND = "bg_incall_screen";
 
+    private static final String INCALL_GLOWPAD_TRANSPARENCY = "incall_glowpad_transparency";
+
     private static final String VM_NUMBERS_SHARED_PREFERENCES_NAME = "vm_numbers";
 
     private static final String BUTTON_SIP_CALL_OPTIONS =
@@ -291,6 +293,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private CheckBoxPreference mButtonAutoRetry;
     private CheckBoxPreference mButtonHAC;
     private CheckBoxPreference mButtonCallUiInBackground;
+    private CheckBoxPreference mIncallGlowpadTransparency;
     private ListPreference mButtonDTMF;
     private ListPreference mButtonTTY;
     private CheckBoxPreference mButtonNoiseSuppression;
@@ -595,6 +598,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (preference == mButtonCallUiInBackground) {
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
                     Settings.System.CALL_UI_IN_BACKGROUND,
+                    (Boolean) objValue ? 1 : 0);
+        } else if (preference == mIncallGlowpadTransparency) {
+            Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.INCALL_GLOWPAD_TRANSPARENCY,
                     (Boolean) objValue ? 1 : 0);
         } else if (preference == mVoicemailProviders) {
             final String newProviderKey = (String) objValue;
@@ -1573,6 +1580,8 @@ public class CallFeaturesSetting extends PreferenceActivity
         mButtonTTY = (ListPreference) findPreference(BUTTON_TTY_KEY);
         mButtonCallUiInBackground =
                 (CheckBoxPreference) findPreference(BUTTON_CALL_UI_IN_BACKGROUND);
+        mIncallGlowpadTransparency =
+                (CheckBoxPreference) findPreference(INCALL_GLOWPAD_TRANSPARENCY);
         mButtonNoiseSuppression = (CheckBoxPreference) findPreference(BUTTON_NOISE_SUPPRESSION_KEY);
         mVoicemailProviders = (ListPreference) findPreference(BUTTON_VOICEMAIL_PROVIDER_KEY);
         if (mVoicemailProviders != null) {
@@ -1641,6 +1650,10 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         if (mButtonCallUiInBackground != null) {
             mButtonCallUiInBackground.setOnPreferenceChangeListener(this);
+        }
+
+        if (mIncallGlowpadTransparency != null) {
+            mIncallGlowpadTransparency.setOnPreferenceChangeListener(this);
         }
 
         if (mButtonNoiseSuppression != null) {
@@ -1893,6 +1906,12 @@ public class CallFeaturesSetting extends PreferenceActivity
             int callUiInBackground = Settings.System.getInt(getContentResolver(),
                     Settings.System.CALL_UI_IN_BACKGROUND, 0);
             mButtonCallUiInBackground.setChecked(callUiInBackground != 0);
+        }
+
+        if (mIncallGlowpadTransparency != null) {
+            int incallGlowpadTrans = Settings.System.getInt(getContentResolver(),
+                    Settings.System.INCALL_GLOWPAD_TRANSPARENCY, 0);
+            mIncallGlowpadTransparency.setChecked(incallGlowpadTrans != 0);
         }
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
